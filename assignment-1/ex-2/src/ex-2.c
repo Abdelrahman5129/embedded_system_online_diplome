4f093fa63277026f2041781b8b11ccaa0ab9cf2e
Abdelrahman5129 <142165069+Abdelrahman5129@users.noreply.github.com>
2023-09-09 19:50:37 +0300

    ex2

---
+++ assignment-1/ex-2/src/ex-2.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : ex-2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+void main(){
+	int x;
+	printf("Enter a integer:\n");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered: %d",x);
+	return 0 ;
+}
