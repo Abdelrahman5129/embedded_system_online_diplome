53d74ea8913e317eb60fb41660f293deb66fc5ba
Abdelrahman5129 <142165069+Abdelrahman5129@users.noreply.github.com>
2023-09-09 19:25:00 +0300

    first assigmnt

---
+++ assignment-1/ex-1/src/ex-1.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : ex-1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+void main()
+{
+	printf("C Programming");
+}
