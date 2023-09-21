accc195ea222594c1adac8dfc0c0e48a422dd5cb
Abdelrahman5129 <142165069+Abdelrahman5129@users.noreply.github.com>
2023-09-21 05:37:59 +0300

    add ex-4-5

--- assignment-2/homework3/array/ex4/src/ex4.c
+++ assignment-2/homework3/array/ex4/src/ex4.c
@@ -13,5 +13,24 @@
 
 int main(void)
 {
-
+int x[50],a,b,c,i;
+printf("enter of elements");
+scanf("%d",&a);
+for(i=0;i<a;++i)
+{
+	scanf("%d",&x[i]);
+}
+printf("enter the element to be inserted");
+scanf("%d",&b);
+printf("enter the location");
+scanf("%d",&c);
+for (i=a;i>=c;i--)
+{
+	x[i] = x[i-1];
+}
+a++;
+x[c - 1] = b;
+for (i=0;i< a;i++)
+	printf("n %d",x[i]);
+return 0;
 }
---
+++ assignment-2/homework3/array/ex5/src/ex5.c
@@ -0,0 +1,40 @@
+/*
+ ============================================================================
+ Name        : ex5.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a[30], ele , num ,i ;
+	 printf("\nEnter no of elements :");
+	 scanf ("%d",&num);
+	 printf("\nEnter the values :");
+	 for (i=0;i<num;i++)
+	 {
+		 scanf("%d ",&a[i]);
+	 }
+	 printf("\nEnter the elements to be searched :");
+	 scanf("%d",&ele);
+	 i=0;
+	 while (i<num && ele != a[i])
+	 {
+		 i++;
+	 }
+	 if (i< num)
+	 {
+		printf("Number found at the location =%d",i+1);
+	 }
+	 else
+	 {
+		printf("Number not found");
+	 }
+	 return 0;
+}
